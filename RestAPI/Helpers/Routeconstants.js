47facc7247795f2e1db6ff59200df86ee1a6b65e
Madhumitha Kadari <madhumithakadari1910@gmail.com>
2020-09-11 13:50:40 +0530

    commit

---
+++ RestAPI/Helpers/Routeconstants.js
@@ -0,0 +1,21 @@
+//exporting an obj
+
+module.exports.ROUTE_CONSTANTS = {
+
+    DEFAULT:"/",
+
+    GET_PRODUCTS:"/products",
+
+    GET_LOGIN:"/login",
+
+    GET_REGISTER:"/register",
+
+    GET_FILE:"/file",
+
+    POST_REGISTER:"/register",
+
+    POST_LOGIN:"/login",
+
+    GET_COUNTRIES:"/countryList"
+
+}
